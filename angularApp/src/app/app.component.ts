236ad098806b31a7e3ce5587c981f34c92ab62d4
anderuraga <ander.uraga@gmail.com>
2020-01-17 09:40:06 +0100

    app angular inicio

---
+++ angularApp/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+
+/**
+ * componente principal que se carga al arrancar la App
+ * 
+ * selector: nombre de la etiqueta para inyectar componente en HTML
+ * templateUrl: Vista del HTML
+ * styleUrls: estilos para la vista formato scss
+ * 
+ */
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  //variable
+  title = 'Angular App';
+
+
+}
