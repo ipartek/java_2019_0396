0123bae0621b513845001c751f099ba11d66113e
anderuraga <ander.uraga@gmail.com>
2020-02-05 09:29:37 +0100

    juego de clicks

--- angularApp/src/app/paginas/juego/juego.component.ts
+++ angularApp/src/app/paginas/juego/juego.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+const TIEMPO_JUEGO = 4000;
+
 @Component({
   selector: 'app-juego',
   templateUrl: './juego.component.html',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JuegoComponent implements OnInit {
 
-  constructor() { }
+  jugador: string;
+  totalClicks: number;
+  ranking: Map<string, number>;
+  isActivo: boolean;
+
+  constructor() {
+    console.trace('JuegoComponent constructor');
+    this.jugador = '';
+    this.totalClicks = 0;
+    this.ranking = new Map();
+    this.isActivo = false;
+
+    /*
+    this.ranking.set('pepe', 100);
+    this.ranking.set('pepa', 3200);
+    this.ranking.set('iñigo', 1100);
+    */
+
+  }// constructor
 
   ngOnInit() {
-  }
+      console.trace('JuegoComponent ngOnInit');
 
-}
+  } // ngOnInit
+
+
+  comenzar(): void {
+    console.trace('comenzar');
+    this.isActivo = true;
+
+    setTimeout( () => {
+      console.debug('Termina timeout');
+      this.ranking.set( this.jugador , this.totalClicks );
+      this.isActivo = false;
+      this.totalClicks = 0;
+      this.jugador = '';
+
+    }, TIEMPO_JUEGO);
+
+
+  }// comenzar
+
+  sumar(): void {
+    console.trace('sumar');
+    this.totalClicks++;
+
+  }// sumar
+
+
+}// JuegoComponent
